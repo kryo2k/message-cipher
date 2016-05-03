400034ec7f0e1be2a2a501206cf6dd3116bf7193
Hans Doller <kryo2k@gmail.com>
2016-05-02 18:05:13 -0600

    Initial project files.

---
+++ README.md
@@ -0,0 +1,83 @@
+## Private/Public key encryption
+
+An exploration of node's crypto functions
+
+## Install
+```bash
+nvm use 5 # works best if run with node 5x
+npm install
+```
+
+### Generate a new private key file
+```bash
+node index.js generate
+```
+
+### Force-regenerate private key file
+```bash
+node index.js generate -f
+```
+
+### Generate private key and dump to output
+```bash
+node index.js generate -R
+```
+
+### Read info about current private key file
+```bash
+node index.js read
+```
+
+### Encrypt a message
+```bash
+node index.js encrypt [--priv <private-key>] [--pub <public-key>] <msg>
+```
+
+### Decrypt a message
+```bash
+node index.js decrypt [--priv <private-key>] [--pub <public-key>] <encrypted-msg>
+```
+
+### Encrypting message to other user
+```bash
+node index.js encrypt [--priv <sender-private-key>] --pub <receiver-public-key> <msg>
+```
+
+### Decrypting message sent by sender to other user
+```bash
+node index.js decrypt [--priv <sender-private-key>] --pub <receiver-public-key> <encrypted-msg>
+```
+
+### Decrypting message from other user
+```bash
+node index.js decrypt [--priv <receiver-private-key>] --pub <sender-public-key> <encrypted-msg>
+```
+
+
+## Example Script
+
+```bash
+export MY_PRI="03ed57857b42c69bf0559087d748d78a83f011d1459274c8f09f0ae3afe25d70f75f83684dbf46b959cf2a403580f5fbf347e18515cd47e9e27c07e87e1ddf74b908477337e5f7ef"
+export MY_PUB="0406baf5bd0db574d342fba3e53619a931652e6f8088c7f415b6af7ee191c1bddb571c4fe2a4ccb61aec940079a4424ef5fc4d222ab3fca22e0d5721511eebfe607820536642a3c71d02f84661b744a890cb8778e145803560ab04b69ee71a8e61b90a4ce3fde92c15e5d54d76e048bbd712475f0a772505acb9458c9d7e8e14c5e6466f937837b11cacfbf55e0def2753"
+
+export TEST_PRI="9f6ba49209d617402b5459f33c2b2f56edcdeb8f021242e2ec54916b9a5485cc6e4852c5061037d14f9ddbbfc8a34638f1730094faffb1a0508a7872eb32fd5d1c40d1b516890c"
+export TEST_PUB="0404832e0a1ba719ed3ae03ba86e356fd1d3bc8df89d824cbd5bdd89439bcf45aae2c65e0f6af0d340638776545ad69fe7fbe948f1d3ad02ef6c94b52b274482e49d14b5d575c423be01df7252962db72c99e3b4c18c87e9f25f16bcbcfde371ca784d0ff2be1697ff96369ff8a9172f0045f30ae90f79c0bea47b51efe1a8ede05a698abb51c58a7494dba9bc3dfef79f"
+
+node index.js encrypt --priv $MY_PRI --pub $TEST_PUB "This is a test"
+
+# Outputs >>
+# Encrypted Message:
+# 1bc089d5d64e87f2c6d1aaf170fc8330a8630eeb85dcb262b30ee465e179749e8616e4192d8acf124335f34bb49e16ac6543f36905dd9ab478bb30f57f2bc6bfd8da326c6704c1335f4611d96e
+
+node index.js decrypt --priv $MY_PRI --pub $TEST_PUB 1bc089d5d64e87f2c6d1aaf170fc8330a8630eeb85dcb262b30ee465e179749e8616e4192d8acf124335f34bb49e16ac6543f36905dd9ab478bb30f57f2bc6bfd8da326c6704c1335f4611d96e
+
+# Outputs >>
+# Decrypted Message ("2016-05-02T23:38:53.936Z"):
+# This is a test
+
+node index.js decrypt --priv $TEST_PRI --pub $MY_PUB 1bc089d5d64e87f2c6d1aaf170fc8330a8630eeb85dcb262b30ee465e179749e8616e4192d8acf124335f34bb49e16ac6543f36905dd9ab478bb30f57f2bc6bfd8da326c6704c1335f4611d96e
+
+# Outputs >>
+# Decrypted Message ("2016-05-02T23:38:53.936Z"):
+# This is a test
+```
---
+++ index.js
@@ -0,0 +1,297 @@
+'use strict';
+
+const yargs  = require('yargs');
+const format = require('util').format;
+const fs     = require('fs');
+const path   = require('path');
+const crypto = require('crypto');
+const pkg    = require('./package.json');
+
+const CURVE = 'sect571r1';
+const CIPHER = 'aes-256-ctr';
+const HASH = 'sha256';
+const FORMAT = 'uncompressed';
+const ENCODING = 'hex';
+const BIN = 'binary';
+const PRIVATE_KEY = path.join(process.env.HOME, '.privatemessenger');
+const SIGLEN = 64;
+
+if(crypto.getCurves().indexOf(CURVE) === -1) {
+  throw new Error(format('The supplied curve (%s) is not available.', CURVE));
+}
+else if(crypto.getCiphers().indexOf(CIPHER) === -1) {
+  throw new Error(format('The supplied cipher (%s) is not available.', CIPHER));
+}
+else if(crypto.getHashes().indexOf(HASH) === -1) {
+  throw new Error(format('The supplied hash (%s) is not available.', HASH));
+}
+
+exports.newEcdh = () => {
+  return crypto.createECDH(CURVE);
+};
+
+exports.newCipher = (secret) => {
+  return crypto.createCipher(CIPHER, secret);
+};
+
+exports.newDecipher = (secret) => {
+  return crypto.createDecipher(CIPHER, secret);
+};
+
+exports.newHash = () => {
+  return crypto.createHash(HASH);
+};
+
+exports.restoreEcdh = (privateKey, fromExport) => {
+  const ecdh = exports.newEcdh();
+  ecdh.setPrivateKey(privateKey, fromExport ? ENCODING : BIN);
+  return ecdh;
+};
+
+exports.restoreEcdhFile = (filename) => {
+  if(!fs.existsSync(filename)) {
+    throw new Error(format('Private key file (%s) does not exist.', filename));
+  }
+
+  const data = fs.readFileSync(filename, { encoding: 'utf8' });
+
+  if(!data) {
+    throw new Error(format('Unable to read contents of private key file (%s).', filename))
+  }
+
+  const ecdh = exports.restoreEcdh(data, true);
+
+  return ecdh;
+};
+
+exports.ecdhKeys = (ecdh, forExport) => {
+  return ecdh.generateKeys(forExport ? ENCODING : BIN, FORMAT);
+};
+
+exports.ecdhCipher = (ecdh, pubkey, fromExport) => {
+  return exports.newCipher(exports.ecdhComputeSecret(ecdh, pubkey, fromExport));
+};
+
+exports.ecdhDecipher = (ecdh, pubkey, fromExport) => {
+  return exports.newDecipher(exports.ecdhComputeSecret(ecdh, pubkey, fromExport));
+};
+
+exports.ecdhPrivateKey = (ecdh, forExport) => {
+  return ecdh.getPrivateKey(forExport ? ENCODING : BIN);
+};
+
+exports.ecdhPublicKey = (ecdh, forExport) => {
+  return ecdh.getPublicKey(forExport ? ENCODING : BIN, FORMAT);
+};
+
+exports.ecdhComputeSecret = (ecdh, pubkey, fromExport, forExport) => {
+  if(false === (pubkey instanceof Buffer)) {
+    pubkey = exports.ecdhPublicKey(ecdh);
+  }
+
+  return ecdh.computeSecret(pubkey, fromExport ? ENCODING : BIN, forExport ? ENCODING : BIN);
+};
+
+const ARGVPUBKEY = (argv, ecdh) => {
+
+  var pubkey = null;
+
+  if(argv.pub) { // load from input
+    pubkey = new Buffer(argv.pub, ENCODING);
+  }
+  else { // self-sign
+    pubkey = exports.ecdhPublicKey(ecdh);
+  }
+
+  return pubkey;
+};
+
+const ARGVEDCH = (argv) => {
+
+  var ecdh = false;
+
+  if(argv.priv) { // load from string
+    ecdh = exports.restoreEcdh(argv.priv, true);
+  }
+  else if(argv.path) { // load from file
+    ecdh = exports.restoreEcdhFile(argv.path, true);
+  }
+
+  return ecdh;
+};
+
+const HASHSTR = (str) => {
+  const hasher = exports.newHash();
+  hasher.update(str);
+  return hasher.digest(ENCODING);
+};
+
+const MSGENCODE = (message) => {
+  return JSON.stringify([message, new Date()]);
+};
+
+const MSGDECODE = (message) => {
+  var parsed = false;
+  try {
+    parsed = JSON.parse(message);
+  }
+  catch(e) {}
+
+  if(!parsed || false === Array.isArray(parsed) || parsed.length < 2) {
+    throw new Error('Message is invalid or not encrypted for the current private/public key pair.')
+  }
+
+  return parsed;
+};
+
+const MSGSIGN = (message) => {
+  if(typeof message !== 'string' || message.length === 0) {
+    throw new Error('Invalid message provided to sign. Must be non-zero length string.');
+  }
+
+  return message + HASHSTR(message);
+};
+
+const MSGUNSIGN = (message) => {
+  if(typeof message !== 'string' || message.length < SIGLEN) {
+    throw new Error(format('Invalid message provided to unsign. Must be a string atleast %d chars long.', SIGLEN));
+  }
+
+  const pos = message.length - SIGLEN;
+  const raw = message.substring(0, pos);
+
+  if(message.substring(pos) !== HASHSTR(raw)) {
+    throw new Error('This message was not signed properly.');
+  }
+
+  return raw;
+};
+
+yargs
+.usage('Usage: $0 <command> [options]')
+.help('h')
+.alias('h', 'help')
+
+.command({
+  command: 'generate [options]',
+  describe: 'Generate a new private key.',
+  builder: {
+    path: {
+      alias: 'p',
+      describe: 'Path to private key',
+      default: PRIVATE_KEY
+    },
+    force: {
+      type: 'boolean',
+      alias: 'f',
+      default: false
+    },
+    'no-write': {
+      type: 'boolean',
+      alias: 'R',
+      default: false
+    }
+  },
+  handler: (argv) => {
+    const readOnly = !!argv['no-write'];
+
+    if(fs.existsSync(argv.path) && (!argv.force && !readOnly)) {
+      throw new Error(format('Private key file (%s) already exists.', argv.path));
+    }
+
+    const ecdh = exports.newEcdh();
+    const ecdh_pub  = exports.ecdhKeys(ecdh, true);
+    const ecdh_priv = exports.ecdhPrivateKey(ecdh, true);
+
+    if(!readOnly) {
+      const file = fs.openSync(argv.path, 'w');
+      fs.writeFileSync(file, ecdh_priv);
+      fs.closeSync(file);
+      console.log('File (%s) was written.', argv.path);
+    }
+    else {
+      console.log('\nPrivate Key (secret):\n%s\n\nPublicKey:\n%s\n', ecdh_priv, ecdh_pub);
+    }
+  }
+})
+
+.command({
+  command: 'read [options]',
+  describe: 'Read information about an existing key.',
+  builder: {
+    path: {
+      alias: 'p',
+      describe: 'Path to private key',
+      default: PRIVATE_KEY
+    }
+  },
+  handler: (argv) => {
+    const ecdh = exports.restoreEcdhFile(argv.path, true);
+    const ecdh_pub  = exports.ecdhPublicKey(ecdh, true);
+    const ecdh_priv = exports.ecdhPrivateKey(ecdh, true);
+
+    console.log('\nPrivate Key (do not share):\n%s\n', ecdh_priv);
+    console.log('Public Key:\n%s\n', ecdh_pub);
+  }
+})
+
+.command({
+  command: 'encrypt [options] <message>',
+  describe: 'Encrypt a message.',
+  builder: {
+    priv: {
+      type: 'string',
+      describe: 'Override private key with hex-encoded private key value.',
+      default: null
+    },
+    pub: {
+      type: 'string',
+      describe: 'Public key identity to encrypt for (if blank, uses public key from private key)'
+    },
+    path: {
+      alias: 'p',
+      describe: 'Path to private key',
+      default: PRIVATE_KEY
+    }
+  },
+  handler: (argv) => {
+    const ecdh = ARGVEDCH(argv);
+    const pubkey = ARGVPUBKEY(argv, ecdh);
+    const cipher = exports.ecdhCipher(ecdh, pubkey);
+    const encrypted = cipher.update(MSGENCODE(argv.message), 'utf8', ENCODING) + cipher.final(ENCODING);
+
+    console.log('\nEncrypted Message:\n%s\n', MSGSIGN(encrypted));
+  }
+})
+
+.command({
+  command: 'decrypt [options] <message>',
+  describe: 'Decrypts a message.',
+  builder: {
+    priv: {
+      type: 'string',
+      describe: 'Override private key with hex-encoded private key value.',
+      default: null
+    },
+    pub: {
+      type: 'string',
+      describe: 'Public key identity to decrypt from (if blank, uses public key from private key)'
+    },
+    path: {
+      alias: 'p',
+      describe: 'Path to private key',
+      default: PRIVATE_KEY
+    }
+  },
+  handler: (argv) => {
+    const ecdh = ARGVEDCH(argv);
+    const pubkey = ARGVPUBKEY(argv, ecdh);
+    const decipher = exports.ecdhDecipher(ecdh, pubkey);
+    const decrypted = decipher.update(MSGUNSIGN(argv.message), ENCODING, 'utf8') + decipher.final('utf8');
+    const loaded = MSGDECODE(decrypted);
+
+    console.log('\nDecrypted Message (%j):\n%s\n', loaded[1], loaded[0]);
+  }
+})
+.epilog(pkg.epilog)
+.argv;
